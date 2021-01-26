d404798a60822c81afd2b0ca91e0a55c170b4989
Brian Silva <brianfsilva011@gmail.com>
2021-01-26 19:52:37 -0400

    implementação da variação de dias do mês (ainda não resolvida)

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Scanner;
 
 import entities.Calendario;
 
@@ -20,9 +21,11 @@ public class Program {
 		System.out.println();
 
 		//teste
+		Scanner sc = new Scanner(System.in);
+		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy"); 
 		SimpleDateFormat sdf1 = new SimpleDateFormat("EE"); 
-		SimpleDateFormat sdf2 = new SimpleDateFormat("MM"); 
+		SimpleDateFormat sdf2 = new SimpleDateFormat("dd"); 
 
 		String[][] teste = new String[6][7];
 		
@@ -30,18 +33,41 @@ public class Program {
 		
 		Calendar cal = Calendar.getInstance();		
 		
+		System.out.print("Digite um mês: ");
+		int mes = sc.nextInt() -1;
+		
+		cal.setTime(d1);
+		cal.add(Calendar.MONTH, mes);
+		d1 = cal.getTime();
+
+		/*System.out.println("Dia: "+day);
+		System.out.println("Mês: "+(month +1));
+		System.out.println("Ult: "+lastDayMonth);*/
+		
 		for(int i=0; i<teste.length; i++) {
 			System.out.println();
 			for (int j=0; j<7; j++) {
-				teste[i][j] = sdf1.format(d1);
-					
-				if(teste[i][j] != null) {
-			    	System.out.print(" "+ sdf1.format(d1));					
-				}
-			    else {
-			    	System.out.print(" - ");		
-			    }
+
+				int day = cal.get(Calendar.DAY_OF_MONTH);
+				int month = cal.get(Calendar.MONTH);	
+				int lastDayMonth = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
 				
+				
+				if(month == (mes)) {
+					if(day <= lastDayMonth) {
+						teste[i][j] = sdf2.format(d1);
+						day++;
+					}
+				}
+				else {
+					teste[i][j] = "--";
+				}
+					
+				//imprimi
+				System.out.print(" "+ teste[i][j]);					
+
+				
+				//add 1 dia
 				cal.setTime(d1);
 				cal.add(Calendar.DAY_OF_MONTH, 1);
 				d1 = cal.getTime();
@@ -49,6 +75,17 @@ public class Program {
 		}
 		
 		
+		/*for(int i=0; i<1; i++) {
+			System.out.println();
+			for (int j=0; j<7; j++) {
+				System.out.print(" "+ teste[i][j]);
+			}
+		}*/
 		
+		/*for(int i=0; i<teste.length; i++) {
+			System.out.println();
+			for (int j=0; j<7; j++) {
+			}
+		}*/
 	}
 }
