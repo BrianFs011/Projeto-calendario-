e1122b3e9741df668ee6100924bea0bf2b52350b
Brian Silva <brianfsilva011@gmail.com>
2021-01-27 11:39:51 -0400

    implementação da variação de dias do mês (resulvida), mas espaços no codigo não resolvidos

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,12 +13,12 @@ public class Program {
 
 	public static void main(String[]args) throws ParseException {
 
-		String[][] diasDoMes = new String[6][7];
+		/*String[][] diasDoMes = new String[6][7];
 		String[][] diasDaSemana = new String[1][7]; 
 				
 		Calendario calendario = new Calendario(diasDoMes, diasDaSemana);
 		System.out.println(calendario);
-		System.out.println();
+		System.out.println();*/
 
 		//teste
 		Scanner sc = new Scanner(System.in);
@@ -26,23 +26,22 @@ public class Program {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy"); 
 		SimpleDateFormat sdf1 = new SimpleDateFormat("EE"); 
 		SimpleDateFormat sdf2 = new SimpleDateFormat("dd"); 
+		SimpleDateFormat sdf3 = new SimpleDateFormat("MM"); 
 
-		String[][] teste = new String[6][7];
+		Date[][] teste = new Date[53][7];
 		
 		Date d1 = Date.from(Instant.parse("2020-12-27T04:00:00Z"));
 		
 		Calendar cal = Calendar.getInstance();		
 		
 		System.out.print("Digite um mês: ");
-		int mes = sc.nextInt() -1;
+		int mesPrint = sc.nextInt();
+		
+		int mes = 0;
 		
 		cal.setTime(d1);
 		cal.add(Calendar.MONTH, mes);
 		d1 = cal.getTime();
-
-		/*System.out.println("Dia: "+day);
-		System.out.println("Mês: "+(month +1));
-		System.out.println("Ult: "+lastDayMonth);*/
 		
 		for(int i=0; i<teste.length; i++) {
 			System.out.println();
@@ -52,40 +51,30 @@ public class Program {
 				int month = cal.get(Calendar.MONTH);	
 				int lastDayMonth = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
 				
-				
-				if(month == (mes)) {
-					if(day <= lastDayMonth) {
-						teste[i][j] = sdf2.format(d1);
-						day++;
-					}
-				}
-				else {
-					teste[i][j] = "--";
-				}
-					
-				//imprimi
-				System.out.print(" "+ teste[i][j]);					
-
-				
+				teste[i][j] = d1;
+								
 				//add 1 dia
 				cal.setTime(d1);
 				cal.add(Calendar.DAY_OF_MONTH, 1);
 				d1 = cal.getTime();
 			}
 		}
+		//print
 		
-		
-		/*for(int i=0; i<1; i++) {
+		System.out.print("dom seg ter qua qui sex sáb");
+		for(int i=0; i<53; i++) {
 			System.out.println();
 			for (int j=0; j<7; j++) {
-				System.out.print(" "+ teste[i][j]);
+
+				if(teste[i][j].getMonth() + 1== mesPrint) {
+					//imprimi
+					System.out.print(" "+ sdf2.format(teste[i][j])+" ");														
+					}
+				else {
+					System.out.print("    ");
+				}
+				
 			}
-		}*/
-		
-		/*for(int i=0; i<teste.length; i++) {
-			System.out.println();
-			for (int j=0; j<7; j++) {
-			}
-		}*/
+		}
 	}
 }
